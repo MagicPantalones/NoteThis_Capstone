f496e32f90f215ac582252e3036b34401b0ec30a
Erik <erik.aandal@gmail.com>
2018-08-08 20:01:29 +0200

    #Initial Commit

--- app/src/main/java/io/magics/notethis/MainActivity.java
+++ app/src/main/java/io/magics/notethis/MainActivity.java
@@ -7,11 +7,6 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
-    private static final String EMAIL = "tester@magics.io";
-    private static final String PASS = "tester";
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
